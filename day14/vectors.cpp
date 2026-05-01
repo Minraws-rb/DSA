b193c3eaa6bb86e8870d81fa5ea2f8b66b245fd1
Minraws-rb <pas081bct047@wrc.edu.np>
2026-05-01 23:59:21 +0545

    day14

--- day14/vectors.cpp
+++ day14/vectors.cpp
@@ -11,8 +11,16 @@ void explainVector(){
     vec.push_back({2,3}); //this needs curly brackets
     vec.emplace_back(4,5);//this doesnt need curly brackets
 
-    vector <int,int> v1(5,100);
-    cout<<|v1[0];
+    vector <int> v1(5,100);{100,100,100,100,100}
+    cout<<v1[0];
+
+    vector <int> v2(5); //{0,0,0,0,0}
+
+    vector<int>::iterator it = v.begin();
+    it++;
+
+    cout<<*(it)<<" ";
+    
 }
 
 int main(){
