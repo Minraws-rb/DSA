58f38e3fb2531653892f685abf78ef6890cd7393
Minraws-rb <pas081bct047@wrc.edu.np>
2026-05-04 23:55:58 +0545

    day17

--- day17/map.cpp
+++ day17/map.cpp
@@ -9,8 +9,17 @@ void explainMap() {
     mpp.emplace(make_pair(2,3));
     mpp.insert({4,5});
 
-    
+    for(auto it : mpp){// for eachloop
+        cout<<it.first<<" "<<it.second<<endl;// prints all the keys and values first is key and second is value
+     }
+    cout<<mpp[1]; // prints 2
 
+    auto it = mpp.find(1);
+    cout<<it->first<<" "; // prints {1,2}
+    cout<<it->second;
+
+    auto it = mpp.upper_bound(2);// same as set uses key for bounding
+    auto it = mpp.lower_bound(3);
 }
 
 int main(){
---
+++ day17/multimapandunorderedmap.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainMultiMapandUnorderedMap() {
+    // Multimap is same as map except one key can store multple values// ie duplicate keys and ordered
+   // cannot be used as cout << mpp[2]
+
+   // Unordered map is unique keys but not ordered
+   //o(1) and in worst case o(n)
+}
+int main(){
+    explainMultiMapandUnorderedMap();
+}
